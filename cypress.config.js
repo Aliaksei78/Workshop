0781886a7e3d3baf905ea705b8ebcbcf87b65c01
Alexei Revenko <96779957+Aliaksei78@users.noreply.github.com>
2022-10-10 00:57:47 +0300

    Update cypress.config.js
    pageLoadTimeout: 700000

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   projectId: '9yjz1s',                              // we got it from dashboard
 
   defaultCommandTimeout: 5000,                      // default = 4000, set just for practice
-  pageLoadTimeout: 70000,                           // default = 60000, set just for practice
+  pageLoadTimeout: 700000,                           // default = 60000, set just for practice
   env: {
     storeUrl: "https://store.google.com"              // url for UI tests
   },
